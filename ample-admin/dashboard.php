3ddfe6387819091dbd6e0267f103fa7c8feadae7
Thalita <thalitazharifa10@gmail.com>
2023-05-23 13:12:40 +0700

    logo dashboard

--- ample-admin/dashboard.php
+++ ample-admin/dashboard.php
@@ -49,14 +49,12 @@
                     <a class="navbar-brand" href="dashboard.php">
                         <!-- Logo icon -->
                         <b class="logo-icon">
-                            <!-- Dark Logo icon -->
-                            <img src="plugins/images/logo-icon.png" alt="homepage" />
                         </b>
                         <!--End Logo icon -->
                         <!-- Logo text -->
                         <span class="logo-text">
                             <!-- dark Logo text -->
-                            <img src="plugins/images/logo-text.png" alt="homepage" />
+                            <img src="../images/logo perisai.jpg" alt="homepage" />
                         </span>
                     </a>
                     <!-- ============================================================== -->
